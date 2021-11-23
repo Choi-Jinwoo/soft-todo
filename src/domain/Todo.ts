ccab5f168d6dca7746ba28f5a9fbc8afea821fd0
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-11-23 11:04:53 +0900

    add: Id property in todo and make Date object in constructor

--- src/domain/Todo.ts
+++ src/domain/Todo.ts
@@ -1,15 +1,18 @@
 export type TodoAttrs = {
+  id: string;
   content: string;
   date: Date | null;
 };
 
 class Todo {
+  public id: string;
   public content: string;
   public date: Date | null;
 
   constructor(todo: TodoAttrs) {
+    this.id = todo.id;
     this.content = todo.content;
-    this.date = todo.date;
+    this.date = todo.date && new Date(todo.date);
   }
 }
 
